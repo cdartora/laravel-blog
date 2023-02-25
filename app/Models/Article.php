5094d38f05282844234417e22f8cdce8a208c290
Carlos Dartora <carlosdalsoler@gmail.com>
2023-02-25 02:24:47 -0300

    feat(model): :sparkles: add guarded and table properties on `Article model`

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -9,5 +9,10 @@ class Article extends Model
 {
     use HasFactory;
 
+    protected $table = 'articles';
+
+
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
     protected $fillable = ['title', 'text', 'image'];
 }
