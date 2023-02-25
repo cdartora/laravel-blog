23d79ad7f62370612c92079844993cba54fa8722
Carlos Dartora <carlosdalsoler@gmail.com>
2023-02-25 00:34:56 -0300

    refactor: :ambulance: just renaming more redirect to `home`

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect(route('homepage'));
+        return redirect(route('home'));
     }
 
     /**
